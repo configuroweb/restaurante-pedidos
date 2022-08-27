932cc4b76eeb75b9e3dc32681ee3d3129fbb1b0c
Mauricio Sevilla Britto <msevillab@gmail.com>
2022-08-27 09:11:12 -0500

    Update initialize.php
    se actualiza url

--- initialize.php
+++ initialize.php
@@ -1,9 +1,9 @@
 <?php
 $dev_data = array('id' => '-1', 'firstname' => 'Mauricio', 'lastname' => 'Sevilla', 'username' => 'configuroweb', 'password' => '4b67deeb9aba04a5b54632ad19934f26', 'last_login' => '', 'date_updated' => '', 'date_added' => '');
-if (!defined('base_url')) define('base_url', 'http://localhost/restaurante/');
+if (!defined('base_url')) define('base_url', 'http://localhost/restaurante-pedidos/');
 if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
 if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
 if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "");
-if (!defined('DB_NAME')) define('DB_NAME', "restaurante");
+if (!defined('DB_NAME')) define('DB_NAME', "restaurante-pedidos");
